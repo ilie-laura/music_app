fbe67446bb4b1102d1a3608c94c1e55e69b5a9ff
laura <ilie_ioana_laura@yahoo.com>
2025-08-25 13:08:48 +0300

    init shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
-
+import { Button } from "@/components/ui/button"
 function App() {
   return (
     <>
       <h1 className="text-red-500 font-bold text-center">Hello world!</h1>
       <p className="text-gray-700 text-center">Welcome to my Tailwind CSS app!</p>
+       <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button variant={"default"}>Click me</Button>
+    </div>
     </>
   )
 }
