23bb515c8bb7ea005324fda0810f0ade80f5f6e5
laura <ilie_ioana_laura@yahoo.com>
2025-08-25 14:30:09 +0300

    models for fct

---
+++ backend/src/models/messageModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    senderID: {
+        type: String,
+       
+        required: true
+    },
+    receiverID: {
+        type: String,
+     
+        required: true
+    },
+        
+    content: {
+        type: String,
+        required: true
+    },
+   
+}, { timestamps: true });
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
