4286146bb52cc35d0ca6eb9b30785f702660874f
laura <ilie_ioana_laura@yahoo.com>
2025-08-25 14:11:26 +0300

    backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRoute.js';
+import adminRoutes from './routes/adminRoute.js';
+import authRoutes from './routes/authRoute.js';
+import songRoutes from './routes/songRoute.js';
+import albumRoutes from './routes/albumRoute.js';
+import statRoutes from './routes/statRoute.js';
+import  connectDB from './lib/db.js';
+dotenv.config();
+
+const PORT=process.env.PORT||5000;
+const app=express();
+
+app.use("/api/users",userRoutes);
+app.use("/api/auth",authRoutes);
+app.use("/api/admin",adminRoutes);
+app.use("/api/songs",songRoutes);
+app.use("/api/albums",albumRoutes);
+app.use("/api/stats",statRoutes);
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
+})
