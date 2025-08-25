e3b7982bb979c77466fb57a7a642f2a77ed925ba
laura <ilie_ioana_laura@yahoo.com>
2025-08-25 12:59:38 +0300

    init tailwind/postcss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 font-bold text-center">Hello world!</h1>
+      <p className="text-gray-700 text-center">Welcome to my Tailwind CSS app!</p>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+import { join } from 'path'
+
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
