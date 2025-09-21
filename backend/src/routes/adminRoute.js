0cd9904646fc8a73de4de9b8d9c0884f5de09971
laura <ilie_ioana_laura@yahoo.com>
2025-09-21 17:30:06 +0300

    slightly optimised

--- backend/src/routes/adminRoute.js
+++ backend/src/routes/adminRoute.js
@@ -1,6 +1,13 @@
 import {Router} from "express";
-import createSong from "../controllers/adminController.js";
+import {protectRoute,requireAdmin} from "../middleware/authMiddleware.js";
+import {checkAdmin} from "../controllers/adminController.js";
+import {createSong,deleteSong,createAlbum,deleteAlbum} from "../controllers/adminController.js";
 const router = Router();
+router.use(protectRoute,requireAdmin);//all routes below are protected and require admin
+router.get("/check",checkAdmin); 
+router.post("/songs", createSong);
+router.delete("/songs/:id", deleteSong);
 
-router.post("/songs", protectRoute, requireAdmin, createSong);
+router.post("/albums", createAlbum);
+router.delete("/albums/:id", deleteAlbum);
 export default router;
