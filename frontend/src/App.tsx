852c80daa5b87b9da087644c248cc5d2d87e53b0
laura <ilie_ioana_laura@yahoo.com>
2025-10-08 15:32:40 +0300

    react-router-dom installed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import { Button } from "@/components/ui/button"
 import { SignInButton,UserButton } from "@clerk/clerk-react"
 import { SignedIn,SignedOut } from "@clerk/clerk-react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage"
+import HomePage from "./pages/home/HomePage"
+
 function App() {
   return (
     <>
@@ -14,6 +18,14 @@ function App() {
           <UserButton />
         </SignedIn>
     </header>
+     <h1 style={{ color: "red" }}>Music App</h1>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<HomePage />} />
+      
+    <Route path="/auth-callback" element={<AuthCallbackPage />} />
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/pages/auth-callback/AuthCallbackPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AuthCallbackPage() {
+  return (
+    <div>
+      <h1 style={{ color: "red" }}>Authentication Callback</h1>
+    </div>
+  )
+}
+
+export default AuthCallbackPage
